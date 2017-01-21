1d86fdb812d2739b6d3401c23689b3ac33189db2
theJollySin <thejollysin@gmail.com>
2017-01-20 19:52:23 -0800

    trying a deeper network

--- src/chap1-mnist-nn.py
+++ src/chap1-mnist-nn.py
@@ -17,6 +17,13 @@ def main():
     training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
     net = network.Network([784, 30, 10])
     net.SGD(training_data, 30, 15, 3, test_data=test_data)
+    ''' And these settings achieved 97% accuracy, but took about
+		4 times longer to run.
+
+    training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+    net = network.Network([784, 90, 60, 30, 10])
+    net.SGD(training_data, 30, 15, 3, test_data=test_data)
+    '''
 
 if __name__ == '__main__':
     main()
